b707a8c42ba21ec8eea9c8aeaf011d9907d8f52c
Arianna Kosten <arianna.kosten@gmail.com>
2019-04-14 21:50:35 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,2 +1,11 @@
-# Don't forget! This file needs to be 'required' in its spec file
-# See README.md for instructions on how to do this
+def fizzbuzz(int)
+  if int / 3
+    return "Fizz"
+  if int / 5
+    return "Buzz"
+  if int/ 15
+    return "FizzBuzz"
+  if int / 4
+    return "nil"
+  end
+end
