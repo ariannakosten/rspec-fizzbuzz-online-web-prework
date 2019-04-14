f37bb7b0d40c5c33e4b127ebdc588bb1dc5a7c77
Arianna Kosten <arianna.kosten@gmail.com>
2019-04-14 22:04:53 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,24 +1,11 @@
 def fizzbuzz(int)
-  def fizzBuzz(number)
-  if number % 3 == 0 && number % 5 == 0
+  if int % 3 == 0 && int % 5 == 0
     "FizzBuzz"
-  elsif number % 3 == 0
+  elsif int % 3 == 0
     "Fizz"
-  elsif number % 5 == 0
+  elsif int % 5 == 0
     "Buzz"
   else
-    number
-    
-    if int % 3 == 0
-    return "Fizz"
-  end
-  if int % 5 == 0
-    return "Buzz"
-  end
-  if int % 3 == 0 && int % 5 == 0
-    return "FizzBuzz"
-  end
-  if int / 4
-    return "nil"
+    "nil"
   end
 end
