4b6ef2966fec0df791a36dc9ea838039657f58a0
Arianna Kosten <arianna.kosten@gmail.com>
2019-04-14 22:03:59 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,5 +1,15 @@
 def fizzbuzz(int)
-  if int % 3 == 0
+  def fizzBuzz(number)
+  if number % 3 == 0 && number % 5 == 0
+    "FizzBuzz"
+  elsif number % 3 == 0
+    "Fizz"
+  elsif number % 5 == 0
+    "Buzz"
+  else
+    number
+    
+    if int % 3 == 0
     return "Fizz"
   end
   if int % 5 == 0
